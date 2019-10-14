1d8c8c25bbafa636cc7c6cc76ad4bda6b4b2a997
Dishit <dishitkotecha@gmail.com>
2019-10-13 23:02:58 -0400

    Phase 2
    RFS instruction Remaining

---
+++ InstructionProcessing/Decoding.java
@@ -0,0 +1,104 @@
+package InstructionProcessing;
+/**
+ * @author Dishit, Peiyu, Zhaoning, Charitha
+ *
+ */
+public class Decoding {
+	
+	//This is the class for Decoding procedure. Decoding class has one function decToBinary
+		//which takes the decimal instruction and decode it into Binary form and store each part in its own place
+		//in instruction array for later access
+		public int[] decToBinary(int decInstruction) {
+			//This function is to take the integer form of Instruction and convert it into binary
+					//form, then divide it into parts and store each part in its own index in instruction array
+			String binaryInstruction = Integer.toBinaryString(decInstruction);
+			int[] instruction = new int[5];
+			for(int i =0;i<5 ;i++) {
+				instruction[i]=0;
+			}
+			while(binaryInstruction.length() < 16) {
+				binaryInstruction = "0" + binaryInstruction;
+			}
+			System.out.println(binaryInstruction);
+			String insFunctionString = binaryInstruction.substring(0, 6); 
+			switch (insFunctionString) {
+			case "000001":
+				instruction[0] = 1;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "000010":
+				instruction[0] = 2;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "000011":
+				instruction[0] = 3;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "101001":
+				instruction[0] = 41;
+				instruction[1]=Integer.parseInt(binaryInstruction.substring(6,8), 2);
+				instruction[2]=Integer.parseInt(binaryInstruction.substring(8,9), 2);
+				instruction[3]=Integer.parseInt(binaryInstruction.substring(9), 2);
+				break;
+			case "101010":
+				instruction[0] = 42;
+				instruction[1]=Integer.parseInt(binaryInstruction.substring(6,8), 2);
+				instruction[2]=Integer.parseInt(binaryInstruction.substring(8,9), 2);
+				instruction[3]=Integer.parseInt(binaryInstruction.substring(9), 2);
+				break;
+			case "001010":
+				instruction[0] = 10;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "001011":
+				instruction[0] = 11;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "001100":
+				instruction[0] = 12;
+				instruction[1]=Integer.parseInt((binaryInstruction.substring(6,8)), 2);
+				instruction[2]=Integer.parseInt((binaryInstruction.substring(8,10)), 2);
+				instruction[3]=Integer.parseInt((binaryInstruction.substring(10, 11)), 2);
+				instruction[4]=Integer.parseInt((binaryInstruction.substring(11)), 2);
+				break;
+			case "001101":
+				instruction[0] = 13;
+				instruction[1]=Integer.parseInt(binaryInstruction.substring(6,8), 2);
+				instruction[2]=Integer.parseInt(binaryInstruction.substring(8,9), 2);
+				instruction[3]=Integer.parseInt(binaryInstruction.substring(9), 2);
+				break;
+			case "001110":
+				instruction[0] = 14;
+				instruction[1]=Integer.parseInt(binaryInstruction.substring(6,8), 2);
+				instruction[2]=Integer.parseInt(binaryInstruction.substring(8,9), 2);
+				instruction[3]=Integer.parseInt(binaryInstruction.substring(9), 2);
+				break;
+			case "001111":
+				instruction[0] = 15;
+				break;
+			case "00":
+				instruction[0]=00;
+			case "000000":
+				instruction[0]=0;
+			default:
+				break;
+			}
+			return instruction;
+			
+		}
+}
---
+++ InstructionProcessing/Encoding.java
@@ -0,0 +1,460 @@
+package InstructionProcessing;
+
+import sun.security.provider.JavaKeyStore.CaseExactJKS;
+
+/**
+ * @author Dishit, Peiyu, Zhaoning, Charitha
+ *
+ */
+public class Encoding {
+	//This is the class for Encoding procedure. Encoding class has two properties, insFunction and addressString,
+	//it has one function insToDec, which takes a string form instruction and convert it into binary form and then 
+	//convert it into decimal format for later use and access
+	String insFunction = new String();
+	String addressString = new String();
+	public int insToDec(String instruction) {
+	switch (instruction.substring(0, 3)) {
+	case "LDR":
+		insFunction = "000001";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(8, 9)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(10)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "STR":
+		insFunction = "000010";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(8, 9)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(10)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "LDA":
+		insFunction = "000011";
+		switch (instruction.substring(4, 5)) {
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(8, 9)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(10)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "LDX":
+		insFunction = "101001";
+		switch (instruction.substring(4, 5)) {
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(8)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "STX":
+		insFunction = "101010";
+		switch (instruction.substring(4, 5)) {
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(8)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "JZ ": //JZ 0,1,0,10
+		insFunction = "001010";
+		switch (instruction.substring(3, 4)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(5, 6)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(7, 8)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(9)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "JNE"://JNE 0,1,0,10
+		insFunction = "001011";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(8, 9)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(10)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "JCC": //JCC 0,1,0,10
+		insFunction = "001100";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		
+		switch (instruction.substring(8, 9)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(10)));
+		while(addressString.length() < 5) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "JMA": //JMA 1,0,10
+		insFunction = "001101";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(9)));
+		while(addressString.length() < 8) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "JSR": //JSR 1,0,10
+		insFunction = "001110";
+		switch (instruction.substring(4, 5)) {
+		case "0":
+			insFunction += "00";
+			break;
+		case "1":
+			insFunction += "01";
+			break;
+		case "2":
+			insFunction += "10";
+			break;
+		case "3":
+			insFunction += "11";
+			break;
+		default:
+			break;
+		}
+		switch (instruction.substring(6, 7)) {
+		case "0":
+			insFunction += "0";
+			break;
+		case "1":
+			insFunction += "1";
+			break;
+		default:
+			break;
+		}
+		addressString = Integer.toBinaryString(Integer.parseInt(instruction.substring(9)));
+		while(addressString.length() < 8) {
+			addressString = "0" + addressString;
+		}
+		break;
+	case "RFS":
+		insFunction = "001111";
+		break;
+	default:
+		break;
+	}
+
+	insFunction += addressString;
+	int decimalInstruction = Integer.parseInt(insFunction, 2);
+	return decimalInstruction;
+	
+}
+}
